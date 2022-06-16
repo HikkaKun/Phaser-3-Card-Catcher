bd3b51a7ae51db18ec3092679f74264e801fc874
HomoHikka <halfideas31@gmail.com>
2022-06-17 00:13:10 +0300

    - Update repository
    - delete template code
    - add Template.ts for scripts & classes

---
+++ src/Plugins/Template.ts
@@ -0,0 +1,23 @@
+//#region import
+//#endregion
+
+//#region class helpers
+//#endregion
+
+//#region private fields
+//#endregion
+
+//#region public fields
+//#endregion
+
+//#region lifecycle callbacks
+//#endregion
+
+//#region private methods
+//#endregion
+
+//#region public methods
+//#endregion
+
+//#region event handlers
+//#endregion
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,10 @@
-import Phaser from 'phaser'
-
-import HelloWorldScene from './scenes/HelloWorldScene'
+import Phaser from 'phaser';
 
 const config: Phaser.Types.Core.GameConfig = {
-	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 200 }
-		}
-	},
-	scene: [HelloWorldScene]
-}
+    type: Phaser.AUTO,
+    width: 640,
+    height: 640,
+    scene: [],
+};
 
-export default new Phaser.Game(config)
+export default new Phaser.Game(config);
